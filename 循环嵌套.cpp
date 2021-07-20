ec0855b084ff1ba64aeb4ac61575a15656374a52
Elias <ghw1347560270@gmail.com>
2021-07-20 16:28:44 +0800

    第二次测试

---
+++ 循环嵌套.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#include <string>
+//#include <Windows.h>
+//
+//using namespace std;
+//
+////一天想我几次?
+////24小时,每秒一次
+//
+//int main(void)
+//{
+//	int count = 0;
+//
+//	for (int h = 0; h < 24; h++)
+//	{
+//		for (int m = 0; m < 60; m++)
+//		{
+//			for (int s = 0; s < 60; s++)
+//			{
+//				count++;
+//				cout << h << ":" << m << ":" << s << "第" << count<< "次想你" << endl;
+//				Sleep(1000);//延时1000毫秒 ---> 休眠1秒
+//			}
+//		}
+//	}
+//
+//	cout << "一天想你" << count << "次" << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
